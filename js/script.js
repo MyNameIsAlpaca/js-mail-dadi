37d4685ca0ad30b9ccc2e8e31daa6e0f9ef6d8c2
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-02-27 12:37:45 +0100

    feat: pseudo-code js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* 
+1 Creare lista di email con accesso al gioco
+2 fare controllo in base alla mail inserita dall'utente
+  Se la mail è presente nel database iniziare gioco
+  se la mail non è presente nel database dare alert errore
+3 Generare un numero a caso da 1 a 6 per l'user
+4 Generare un numero a caso da 1 a 6 per il pc
+5 confrontare i due risultati
+  Se il numero dell'user è maggiore di quello del pc assegnare 1 punto all'user
+  Se il numero del pc è maggiore di quello del pc assegnare 1 punto al pc
+6 Se il punteggio di uno dei due giocatori è ugale a 2 decretare il vincitore
+*/
+
